7a10050b81d250eb89cd7856c461543b29b24bf2
Pedro Colares <siqueira.pedro1998@gmail.com>
2022-05-04 19:46:21 -0300

    Add server side

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapters";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "d1d52c85ad1e18",
+    pass: "dad92fa205d1b6",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Pedro Colares <siqueira.pedro1998@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
